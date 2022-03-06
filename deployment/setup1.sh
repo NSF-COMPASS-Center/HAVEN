2854ecbe334a89f5507b64565e6bc1dda49c0871
Andrew Chan <andrewclchan211@vt.edu>
2022-03-06 17:11:25 -0500

    comments

--- deployment/setup1.sh
+++ deployment/setup1.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # Run this with bash -i ./setup1.sh before running next setup file
+# You should exit and re-ssh after using this script.
+
+# Load libs
 module purge
 module load apps site/tinkercliffs-rome/easybuild/setup Anaconda3
 
+# Init conda
 conda init
+
+# Kick user off ssh
 exit
