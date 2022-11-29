d230a71c4a2543ab312eb3a26de02e0a757a3e53
Blessy Antony <blessy.antony@tsgforce.com>
2022-11-28 22:19:41 -0500

    random forest implementation

--- src/prediction/models/random_forest.py
+++ src/prediction/models/random_forest.py
@@ -1,22 +1,21 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import GridSearchCV
 import random
 
 
-def run(X_train, X_test, y_train, lr_settings):
-    lr_model = LogisticRegression(solver="sag", penalty="l2", class_weight="balanced", max_iter=1000)
-    classification_type = lr_settings["classification_type"]
+def run(X_train, X_test, y_train, rf_settings):
+    rf_model = RandomForestClassifier(class_weight="balanced")
+    classification_type = rf_settings["classification_type"]
     if classification_type == "multi":
-        print("Multiclass Logistic Regression Model")
+        print("Multiclass Random Forest Model")
         # multinomial: multi-class cross-entropy loss
-        lr_model.multi_class = "multinomial"
 
     # K-Fold Cross Validation: START #
     # hyper-parameter tuning using K-Fold Cross Validation with K = 5;
     # shuffle the data with given random seed before splitting into batches
-    tuning_parameters = {"C": lr_settings["C"]}
+    tuning_parameters = {"n_estimators": rf_settings["n_estimators"], "max_depth": rf_settings["max_depth"]}
     scoring_param = "accuracy"
     print(f"Tuning hyper-parameters {tuning_parameters} based on {scoring_param}")
 
@@ -25,7 +24,7 @@ def run(X_train, X_test, y_train, lr_settings):
     kfold_cv_model = StratifiedKFold(n_splits=5, shuffle=True, random_state=random.randint(0, 10000))
 
     # refit=True : retrain the best model on the full training dataset
-    cv_model = GridSearchCV(estimator=lr_model, param_grid=tuning_parameters, scoring=scoring_param,
+    cv_model = GridSearchCV(estimator=rf_model, param_grid=tuning_parameters, scoring=scoring_param,
                             cv=kfold_cv_model, verbose=2, return_train_score=True, refit=True)
     cv_model.fit(X_train, y_train)
 
@@ -36,19 +35,11 @@ def run(X_train, X_test, y_train, lr_settings):
     # K-Fold Cross Validation: END #
 
     y_pred = classifier.predict_proba(X_test)
-    model_coefficients = get_coefficients(classifier, classification_type)
-    return y_pred, model_coefficients
-
-
-def get_coefficients(model, classification_type):
-    # ndarray of shape (1, n_features) or (n_classes, n_features)
-    coefficients = model.coef_
-    feature_names = model.feature_names_in_
-    if classification_type == "multi":
-        classes = model.classes_
-        model_coefficients = pd.DataFrame(coefficients, columns=feature_names, index=classes)
-    else:
-        model_coefficients = pd.DataFrame(coefficients, columns=feature_names)
-    return model_coefficients
+    # classifier.feature_importances_ : ndarray of shape (n_features, 1)
+    # convert to pandas dataframe of shape (1, n_features)
+    feature_names = classifier.feature_names_in_
+    n_features = len(feature_names)
+    feature_importances = pd.DataFrame(classifier.feature_importances_.reshape(1, n_features), columns=classifier.feature_names_in_)
+    return y_pred, feature_importances
 
 
