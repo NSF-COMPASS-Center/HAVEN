125a5cae67aaae9027dfa71669b4fcaddf8592a0
Blessy Antony <blessy.antony@tsgforce.com>
2023-05-30 19:04:12 -0400

    added jupyter notebook for custom result analysis

--- src/prediction/models/nlp/embedding.py
+++ src/prediction/models/nlp/embedding.py
@@ -5,28 +5,28 @@ from utils import nn_utils
 from prediction.models.nlp.positional_encoding import PositionalEncoding
 
 
-class EmbeddingLayer(nn.Module):
-    def __init__(self, vocab_size, max_seq_len, dim):
-        super(EmbeddingLayer, self).__init__()
-        self.token_embedding = nn.Embedding(vocab_size, dim)
-        self.positional_embedding = nn.Embedding(max_seq_len, dim)
-
-    def forward(self, X):
-        tokens = self.token_embedding(X)
-        b, n, dim = tokens.size()
-        positions = self.positional_embedding(torch.arange(n, device=nn_utils.get_device()))[None, :, :].expand(b, n, dim)
-        return tokens + positions
-
-
 # class EmbeddingLayer(nn.Module):
 #     def __init__(self, vocab_size, max_seq_len, dim):
 #         super(EmbeddingLayer, self).__init__()
 #         self.token_embedding = nn.Embedding(vocab_size, dim)
-#         self.positional_embedding = PositionalEncoding(dim, max_seq_len)
+#         self.positional_embedding = nn.Embedding(max_seq_len, dim)
 #
 #     def forward(self, X):
 #         tokens = self.token_embedding(X)
-#         return self.positional_embedding(tokens)
+#         b, n, dim = tokens.size()
+#         positions = self.positional_embedding(torch.arange(n, device=nn_utils.get_device()))[None, :, :].expand(b, n, dim)
+#         return tokens + positions
+
+
+class EmbeddingLayer(nn.Module):
+    def __init__(self, vocab_size, max_seq_len, dim):
+        super(EmbeddingLayer, self).__init__()
+        self.token_embedding = nn.Embedding(vocab_size, dim)
+        self.positional_embedding = PositionalEncoding(dim, max_seq_len)
+
+    def forward(self, X):
+        tokens = self.token_embedding(X)
+        return self.positional_embedding(tokens)
 
 
 # class ConvolutionEmbeddingLayer(nn.Module):
