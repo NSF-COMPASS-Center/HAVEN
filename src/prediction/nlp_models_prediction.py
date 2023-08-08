38d7a9f58b703bbe8f7ebef9ddfdb5f253e4ae44
Blessy Antony <blessy.antony@tsgforce.com>
2023-08-08 19:00:18 -0400

    renamed evaluation class files for consistency

--- src/prediction/nlp_models_prediction.py
+++ src/prediction/nlp_models_prediction.py
@@ -29,7 +29,7 @@ def execute(input_settings, output_settings, classification_settings):
     sequence_settings = classification_settings["sequence_settings"]
     n_iters = classification_settings["n_iterations"]
 
-    id_col = sequence_settings["id_col"]
+    # id_col = sequence_settings["id_col"]
     sequence_col = sequence_settings["sequence_col"]
     label_col = label_settings["label_col"]
     results = {}
@@ -37,7 +37,8 @@ def execute(input_settings, output_settings, classification_settings):
         print(f"Iteration {iter}")
         # 1. Read the data files
         df = utils.read_dataset(input_dir, input_file_names,
-                                cols=[id_col, sequence_col, label_col])
+                                cols=[sequence_col, label_col])
+                                # cols=[id_col, sequence_col, label_col])
         # 2. Transform labels
         df, index_label_map = utils.transform_labels(df, label_settings,
                                                            classification_type=classification_settings["type"])
