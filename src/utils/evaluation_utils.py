d6521a661e60715c6e3d8ce54dbc07fd1a454119
Blessy Antony <blessy.antony@tsgforce.com>
2023-11-13 18:49:02 -0500

    added f1_score to monitoring

--- src/utils/evaluation_utils.py
+++ src/utils/evaluation_utils.py
@@ -1,6 +1,6 @@
-from sklearn.metrics import auc, precision_recall_curve
+from sklearn.metrics import auc, precision_recall_curve, f1_score
 import pandas as pd
-
+import torch
 
 # compute class-wise auprc in given df
 def compute_class_auprc(df, y_pred_columns, y_true_col):
@@ -11,3 +11,25 @@ def compute_class_auprc(df, y_pred_columns, y_true_col):
         pr_curves.append(pd.DataFrame({"precision": precision, "recall": recall, "class": y_pred_column}))
         auprcs.append({"class": y_pred_column, "auprc": auc(recall, precision)})
     return pd.concat(pr_curves, ignore_index=True), pd.DataFrame(auprcs)
+
+
+def get_f1_score(y_true, y_pred, select_non_zero=False):
+    # convert the y_pred from b x n_classes x n to b x n x n_classes
+    y_pred = y_pred.transpose(1, 2)
+
+    # compute softmax over the predictions
+    y_pred = torch.nn.functional.log_softmax(y_pred, dim=2)
+
+    # select the class index with the highest prediction as the class prediction for that sequence
+    y_pred_vals, y_pred_index = torch.max(y_pred, dim=2)
+    y_pred = y_pred_index
+
+    if select_non_zero:
+        non_zero_indices = torch.nonzero(y_true, as_tuple=True)
+        y_true = y_true[non_zero_indices]
+        y_pred = y_pred[non_zero_indices]
+
+    f1_micro = f1_score(y_true, y_pred, average="micro")
+    f1_macro = f1_score(y_true, y_pred, average="macro")
+    return f1_micro, f1_macro
+
