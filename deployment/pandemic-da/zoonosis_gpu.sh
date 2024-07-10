2b3bb4ab1f136182dcc1b795dd9e513c1a248663
Blessy Antony <blessy.antony@tsgforce.com>
2024-07-10 11:44:19 -0400

    updated configs

--- deployment/pandemic-da/zoonosis_gpu.sh
+++ deployment/pandemic-da/zoonosis_gpu.sh
@@ -7,6 +7,7 @@ conda info
 # Execute python script
 SCRIPT_LOCATION=./src/zoonosis.py
 CONFIG_FILE=$1
+GPU_DEVICE=$2
 
 LOGS_DIR=output/logs
 LOG_FILE=$LOGS_DIR/$(date +%Y_%b_%d_%H_%M_%s).log
@@ -15,7 +16,7 @@ echo "Config File: $CONFIG_FILE"
 echo "Log File: $LOG_FILE"
 
 # set GPU device
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=$GPU_DEVICE
 echo "GPU check"
 python -c "import torch; print(f'GPU available: {torch.cuda.is_available()}\n Available GPU devices: {torch.cuda.device_count()}')"
 echo "Zoonosis NLP models START"
