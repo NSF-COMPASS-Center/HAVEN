a01ad756289e42da7845b61b6cb33c39e7523904
Blessy Antony <blessy.antony@tsgforce.com>
2024-07-22 17:30:29 -0400

    lstm re-exec

--- src/models/nlp/lstm.py
+++ src/models/nlp/lstm.py
@@ -24,11 +24,11 @@ class LSTM_Model(nn.Module):
         hidden_input = self.init_zeros(batch_size=X.size(0))
         cell_input = self.init_zeros(batch_size=X.size(0))
 
-        # return values from rnn:
+        # return values from lstm: output, (hidden_output, cell_output)
         # output: output features from the last layer for each token: num_lstm_layers x batch_size X sequence_length X hidden_dim
         # hidden_output: final hidden state (embedding) for each sequence: num_lstm_layers x batch_size X hidden_dim
         # cell_output: final cell state (embedding) for each sequence: num_lstm_layers x batch_size X hidden_dim
-        output, (hidden_output, cell_output) = self.lstm(X, (hidden_input, cell_input))
+        output, _ = self.lstm(X, (hidden_input, cell_input))
 
         # aggregate the embeddings from lstm
         # mean of the representations of all tokens
