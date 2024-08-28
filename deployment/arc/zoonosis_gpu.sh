e8ddbe52dcabf0e05cf1aef11c06ca4da2cfce61
Blessy Antony <blessy.antony@tsgforce.com>
2024-08-28 18:11:53 -0400

    cov - s prot fine tuning

--- deployment/arc/zoonosis_gpu.sh
+++ deployment/arc/zoonosis_gpu.sh
@@ -4,10 +4,10 @@
 #SBATCH --account=seqevol
 #SBATCH --partition=a100_normal_q
 
-#SBATCH --mem=300G
+#SBATCH --mem=250G
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
-#SBATCH -t 96:00:00 # wall-time required (# 144hrs = 6 days)
+#SBATCH -t 72:00:00 # wall-time required (# 144hrs = 6 days)
 
 
 # Load modules
