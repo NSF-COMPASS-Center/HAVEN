fc5b0cd3a58d803b1a7bdd71ff512645e710c82e
Blessy Antony <blessy.antony@tsgforce.com>
2024-10-22 17:13:34 -0400

    fsl configs

--- deployment/arc/zoonosis_gpu.sh
+++ deployment/arc/zoonosis_gpu.sh
@@ -4,7 +4,7 @@
 #SBATCH --account=seqevol
 #SBATCH --partition=dgx_normal_q
 
-#SBATCH --mem=300G
+#SBATCH --mem=400G
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH -t 120:00:00 # wall-time required (# 144hrs = 6 days)
