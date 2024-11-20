1d119c7c5b92f1f1b5f18bc770ec8911767e3b05
Blessy Antony <blessy.antony@tsgforce.com>
2024-11-20 16:33:22 -0500

    esm2

--- src/datasets/collations/custom_collate_function.py
+++ src/datasets/collations/custom_collate_function.py
@@ -1,10 +1,8 @@
 import torch
 
 class ESM2CollateFunction:
-
     def __call__(self, batch):
-        # batch is a list of ((name, sequence), label) tuples
-        named_sequences = [(item[0][0], item[0][1]) for item in batch]  # (name, sequence)
-        labels = torch.stack([item[1] for item in batch])  # labels
-
-        return named_sequences, labels
+        # zip(*batch) returns
+        # if id_col is included, then ids, named_sequences, labels
+        # else named_sequences, labels
+        return zip(*batch)
