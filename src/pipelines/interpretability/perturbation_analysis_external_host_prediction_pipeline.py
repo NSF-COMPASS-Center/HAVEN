a7276deefa0403f3abb82eab528e3f94a41daff4
Blessy Antony <blessy.antony@tsgforce.com>
2024-11-26 03:33:05 -0500

    bug fix

--- src/pipelines/interpretability/perturbation_analysis_external_host_prediction_pipeline.py
+++ src/pipelines/interpretability/perturbation_analysis_external_host_prediction_pipeline.py
@@ -7,7 +7,6 @@ import tqdm
 
 from utils import utils, dataset_utils, nn_utils, constants, mapper, perturbation_analysis_utils
 from models.baseline.nlp.transformer.transformer import TransformerEncoder
-from few_shot_learning.prototypical_network_few_shot_classifier import PrototypicalNetworkFewShotClassifier
 
 
 def execute(config):
