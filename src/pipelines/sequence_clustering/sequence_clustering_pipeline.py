81d0a6e3d5bd6fad477abc5644ef78dde452ec5d
Kateland <sipek@vt.edu>
2025-03-18 10:57:18 -0400

    running cd-hit locally

--- src/pipelines/sequence_clustering/sequence_clustering_pipeline.py
+++ src/pipelines/sequence_clustering/sequence_clustering_pipeline.py
@@ -1,4 +1,6 @@
 import subprocess
+import os
+from pathlib import Path
 
 def execute(config):
     # input settings
@@ -11,42 +13,46 @@ def execute(config):
     output_settings = config["output_settings"]
     output_dir = output_settings["output_dir"]
     results_dir = output_settings["results_dir"]
+    print("RESULTS DIR")
+    print(results_dir)
     sub_dir = output_settings["sub_dir"]
     output_prefix = output_settings["prefix"]
     output_prefix = output_prefix if output_prefix is not None else ""
 
     # task settings
     task_settings = config["task_settings"]
-    id = task_settings["id"]
-    name = task_settings["name"]
-    sequence_type = task_settings["sequence_type"]
-    threshold = task_settings["threshold"]
-    word_size = task_settings["word_size"]
+    id = task_settings[0]["id"]
+    name = task_settings[0]["name"]
+    sequence_type = task_settings[0]["sequence_type"]
+    threshold = task_settings[0]["threshold"]
+    word_size = task_settings[0]["word_size"]
 
     # Path to store output file
-    input_file_path = os.path.join(input_dir, input_file_name)
-    output_file_name = input_file + output_prefix
-    output_file_path = os.path.join(output_dir, output_file_name)
+    input_file = input_dir + "/" + input_file_name[0]
+    input_file_path = os.path.join(input_file)
+    output_file_name = (output_dir + "/" + results_dir + "/" +
+                        input_file_name[0].replace(".fasta", "") + "_" + output_prefix)
+    output_file_path = os.path.join(output_file_name)
     Path(os.path.dirname(output_file_path)).mkdir(parents=True, exist_ok=True)
 
     # Run the correct sequence clustering algorithm:
     if name == "CD-HIT":
-        run_cd_hit()
+        run_cd_hit(sequence_type, alg_file_path, input_file_path, output_file_path, threshold, word_size)
     elif name == "MMseqs2":
         run_mmseqs2()
     else:
         print("Incorrect name")
     # process_file(input_file_path, output_file_path, label_names)
 
-def run_cd_hit():
+def run_cd_hit(sequence_type, alg_file_path, input_file_path, output_file_path, threshold, word_size):
     if sequence_type == "Protein":
-        cd_hit = algorithm_file_path + "cd-hit"
+        cd_hit = alg_file_path + "/cd-hit"
         subprocess.run([cd_hit, "-i", input_file_path, "-o", output_file_path,
-                        "-c", threshold, "-n", word_size])
+                        "-c", str(threshold), "-n", str(word_size)])
     elif sequence_type == "Nucleotide":
-        cd_hit = algorithm_file_path + "cd-hit-est"
+        cd_hit = alg_file_path + "/cd-hit-est"
         subprocess.run([cd_hit, "-i", input_file_path, "-o", output_file_path,
-                        "-c", threshold, "-n", word_size])
+                        "-c", str(threshold), "-n", str(word_size)])
     else:
         print("Sequence type not supported")
 
--- src/utils/mapper.py
+++ src/utils/mapper.py
@@ -8,53 +8,53 @@ from models.virprobert.virprobert import VirProBERT
 from models.virprobert.ablation.virprobert_wo_hierattn import VirProBERT_wo_HierAttn
 
 from models.virprobert.ablation.bert_virus_host_prediction import BERT_VirusHostPrediction
-from models.external.prostt5_host_prediction import ProstT5_VirusHostPrediction
-from models.external.prott5_host_prediction import ProtT5_VirusHostPrediction
-from models.external.esm2_host_prediction import ESM2_VirusHostPrediction
+# from models.external.prostt5_host_prediction import ProstT5_VirusHostPrediction
+# from models.external.prott5_host_prediction import ProtT5_VirusHostPrediction
+# from models.external.esm2_host_prediction import ESM2_VirusHostPrediction
 #from models.external.esm3_host_prediction import ESM3_VirusHostPrediction
 
 from datasets.protein_sequence_custom_dataset import ProteinSequenceProstT5Dataset
 from datasets.protein_sequence_custom_dataset import ProteinSequenceProtT5Dataset
 from datasets.protein_sequence_custom_dataset import ProteinSequenceESM2Dataset
 
-from datasets.collations.custom_collate_function import ESM2CollateFunction
+# from datasets.collations.custom_collate_function import ESM2CollateFunction
 
-from pipelines.virus_host_prediction_training import fine_tuning_pipeline, fine_tuning_external_pipeline, baseline_deep_learning_pipeline, baseline_machine_learning_pipeline
-from pipelines.transfer_learning import masked_language_modeling_pipleine
-from pipelines.analysis import perturbation_analysis_pipeline, perturbation_analysis_external_pipeline, embedding_generation_pipeline, virus_host_prediction_testing_pipeline, virus_host_prediction_testing_external_pipeline
-from pipelines.few_shot_learning import few_shot_learning_host_prediction_pipeline
-from pipelines.evaluation import evaluation_pipeline
+# from pipelines.virus_host_prediction_training import fine_tuning_pipeline, fine_tuning_external_pipeline, baseline_deep_learning_pipeline, baseline_machine_learning_pipeline
+# from pipelines.transfer_learning import masked_language_modeling_pipleine
+# from pipelines.analysis import perturbation_analysis_pipeline, perturbation_analysis_external_pipeline, embedding_generation_pipeline, virus_host_prediction_testing_pipeline, virus_host_prediction_testing_external_pipeline
+# from pipelines.few_shot_learning import few_shot_learning_host_prediction_pipeline
+# from pipelines.evaluation import evaluation_pipeline
 from pipelines.sequence_clustering import sequence_clustering_pipeline
 
 pipeline_mapper = {
-    "masked_language_modeling": masked_language_modeling_pipleine,
-    "virus_host_prediction": fine_tuning_pipeline,
-    "virus_host_prediction_external": fine_tuning_external_pipeline,
-    "virus_host_prediction_baseline_deep_learning": baseline_deep_learning_pipeline,
-    "virus_host_prediction_baseline_machine_learning": baseline_machine_learning_pipeline,
-    "virus_host_prediction_test": virus_host_prediction_testing_pipeline,
-    "virus_host_prediction_test_external": virus_host_prediction_testing_external_pipeline,
-    "few_shot_learning": few_shot_learning_host_prediction_pipeline,
-    "evaluation": evaluation_pipeline,
-    "perturbation": perturbation_analysis_pipeline,
-    "perturbation_external": perturbation_analysis_external_pipeline,
-    "embedding_generation": embedding_generation_pipeline,
+    # "masked_language_modeling": masked_language_modeling_pipleine,
+    # "virus_host_prediction": fine_tuning_pipeline,
+    # "virus_host_prediction_external": fine_tuning_external_pipeline,
+    # "virus_host_prediction_baseline_deep_learning": baseline_deep_learning_pipeline,
+    # "virus_host_prediction_baseline_machine_learning": baseline_machine_learning_pipeline,
+    # "virus_host_prediction_test": virus_host_prediction_testing_pipeline,
+    # "virus_host_prediction_test_external": virus_host_prediction_testing_external_pipeline,
+    # "few_shot_learning": few_shot_learning_host_prediction_pipeline,
+    # "evaluation": evaluation_pipeline,
+    # "perturbation": perturbation_analysis_pipeline,
+    # "perturbation_external": perturbation_analysis_external_pipeline,
+    # "embedding_generation": embedding_generation_pipeline,
     "sequence_clustering": sequence_clustering_pipeline,
 }
 
 # mappings of all classes
 model_map = {
-    "FNN": FNN_VirusHostPrediction,
-    "CNN": CNN_1D_VirusHostPrediction,
-    "RNN": RNN_VirusHostPrediction,
-    "LSTM": LSTM_VirusHostPrediction,
-    "Transformer_Encoder": TransformerEncoderVirusHostPrediction,
-    "BERT": BERT_VirusHostPrediction,
-    "VirProBERT_wo_Hierarchical_Attention": VirProBERT_wo_HierAttn,
-    "VirProBERT": VirProBERT,
-    "ProstT5": ProstT5_VirusHostPrediction,
-    "ProtT5": ProtT5_VirusHostPrediction,  # IMP: note the difference between ProstT5 and ProtT5
-    "ESM2": ESM2_VirusHostPrediction,
+    # "FNN": FNN_VirusHostPrediction,
+    # "CNN": CNN_1D_VirusHostPrediction,
+    # "RNN": RNN_VirusHostPrediction,
+    # "LSTM": LSTM_VirusHostPrediction,
+    # "Transformer_Encoder": TransformerEncoderVirusHostPrediction,
+    # "BERT": BERT_VirusHostPrediction,
+    # "VirProBERT_wo_Hierarchical_Attention": VirProBERT_wo_HierAttn,
+    # "VirProBERT": VirProBERT,
+    # "ProstT5": ProstT5_VirusHostPrediction,
+    # "ProtT5": ProtT5_VirusHostPrediction,  # IMP: note the difference between ProstT5 and ProtT5
+    # "ESM2": ESM2_VirusHostPrediction,
 #    "ESM3": ESM3_VirusHostPrediction
 }
 
@@ -66,5 +66,5 @@ dataset_map = {
 }
 
 collate_function_map = {
-    "ESM2": ESM2CollateFunction
+    # "ESM2": ESM2CollateFunction
 }
