4def9005172755a47d7f5a3914f8251ec384cd4e
Kateland <sipek@vt.edu>
2025-04-02 14:09:35 -0400

    baseline project debugging

--- src/models/virprobert/virprobert_embeddings.py
+++ src/models/virprobert/virprobert_embeddings.py
@@ -20,6 +20,7 @@ class VirProBERT_Emb(ProteinSequenceClassification):
 
     def forward(self, X, embedding_only = True):
         self.input_embedding = get_embedding(X)
+        print("TEST  - FORWARD IN VIRPROBERT")
         return self.input_embedding
         # return super().forward(X, embedding_only = embedding_only)
 
