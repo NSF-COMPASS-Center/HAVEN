e9de9b6f59eeba1b691818feeb7a0d84671a8a3c
Kateland <sipek@vt.edu>
2025-04-23 09:09:18 -0400

    baseline project debugging

--- deployment/arc/run_pipeline_gpu.sh
+++ deployment/arc/run_pipeline_gpu.sh
@@ -21,7 +21,8 @@ module load Miniconda3
 module load cuda12.4/toolkit
 
 # Load conda environment
-source activate ~/.conda/envs/virprobert
+#source activate ~/.conda/envs/virprobert
+source activate /home/sipek/.conda/envs/virprobert
 echo "Conda information:"
 conda info
 
