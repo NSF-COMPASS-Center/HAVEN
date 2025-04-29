cd00ddccf14a2785c59b623854b83cd283cdf686
Kateland <sipek@vt.edu>
2025-04-29 12:45:39 -0400

    model test

--- src/evaluation/multi_class_evaluation.py
+++ src/evaluation/multi_class_evaluation.py
@@ -59,10 +59,11 @@ class MultiClassEvaluation(EvaluationBase):
         print("AUPRC")
         print("y pred columns", self.y_pred_columns)
         for y_pred_column in self.y_pred_columns:
-            print(df_itr[self.y_true_col].values)
-            print(df_itr[y_pred_column].values)
-            print(y_pred_column)
-            precision, recall, _ = precision_recall_curve(y_true=df_itr[self.y_true_col].values, y_score=df_itr[y_pred_column].values, pos_label=y_pred_column)
+            y_true = df_itr[self.y_true_col].values
+            y_score = df_itr[y_pred_column].values
+
+            # precision, recall, _ = precision_recall_curve(y_true=df_itr[self.y_true_col].values, y_score=df_itr[y_pred_column].values, pos_label=y_pred_column)
+            precision, recall, _ = precision_recall_curve(y_true, y_score)
             print("Precision", precision)
             print("Recall", recall)
             pr_curves.append(pd.DataFrame({"precision": precision, "recall": recall, self.class_col: y_pred_column}))
