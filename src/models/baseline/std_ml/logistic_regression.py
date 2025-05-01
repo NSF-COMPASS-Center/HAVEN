1aadc945fe44377d0b7e59dcdc61694d459cbae9
Kateland <sipek@vt.edu>
2025-05-01 15:24:30 -0400

    standardize + pca lr

--- src/models/baseline/std_ml/logistic_regression.py
+++ src/models/baseline/std_ml/logistic_regression.py
@@ -49,10 +49,11 @@ def run(X_train, X_test, y_train, lr_settings):
     # K-Fold Cross Validation: END #
 
     y_pred = classifier.predict_proba(X_test)
-    model_coefficients = get_coefficients(classifier, classification_type)
+    # model_coefficients = get_coefficients(classifier, classification_type)
     validation_scores = utils.get_validation_scores(cv_model.cv_results_)
 
-    return y_pred, model_coefficients, validation_scores, classifier
+    # return y_pred, model_coefficients, validation_scores, classifier
+    return y_pred,[], validation_scores, classifier
 
 
 def get_coefficients(model, classification_type):
