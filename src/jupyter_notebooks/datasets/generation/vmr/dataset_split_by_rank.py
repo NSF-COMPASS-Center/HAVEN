0e6e83f55c09dd215880fc88453b273b73d95abb
Kateland <sipek@vt.edu>
2026-04-02 12:19:03 -0400

    predicting genus using ViTax RefSeq-based dataset splitting

---
+++ src/jupyter_notebooks/datasets/generation/vmr/dataset_split_by_rank.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df = pd.read_csv("/Users/katelandsipe/Documents/Research/git/viral_tax_dataset/vmr_msl40_v2_20251013_proteins_taxonomy_final_gt1.csv")
+output_file_path = "/Users/katelandsipe/Documents/Research/git/viral_tax_dataset/splits/"
+null_seqs = df["prot_seq"].isna().sum()
+print(f"Number of NA Prot Sequences: {null_seqs}")
+
+df = df.dropna(subset=["prot_seq"]).reset_index(drop=True)
+
+
+for genus, group in df.groupby("Genus"):
+    group.to_csv(f"{output_file_path}Genus/{genus}.csv", index=False)
+
+for order, group in df.groupby("Order"):
+    group.to_csv(f"{output_file_path}Order/{order}.csv", index=False)
+
+for family, group in df.groupby("Family"):
+    group.to_csv(f"{output_file_path}Family/{family}.csv", index=False)
