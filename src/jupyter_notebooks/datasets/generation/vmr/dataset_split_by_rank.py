d187fa4d9c3030e45e3310251f28d04ff29c9f49
Kateland <sipek@vt.edu>
2026-04-02 13:36:23 -0400

    predicting genus using ViTax RefSeq-based dataset splitting

--- src/jupyter_notebooks/datasets/generation/vmr/dataset_split_by_rank.py
+++ src/jupyter_notebooks/datasets/generation/vmr/dataset_split_by_rank.py
@@ -11,8 +11,21 @@ df = df.dropna(subset=["prot_seq"]).reset_index(drop=True)
 for genus, group in df.groupby("Genus"):
     group.to_csv(f"{output_file_path}Genus/{genus}.csv", index=False)
 
+for family, group in df.groupby("Family"):
+    group.to_csv(f"{output_file_path}Family/{family}.csv", index=False)
+
 for order, group in df.groupby("Order"):
     group.to_csv(f"{output_file_path}Order/{order}.csv", index=False)
 
-for family, group in df.groupby("Family"):
-    group.to_csv(f"{output_file_path}Family/{family}.csv", index=False)
+
+for class_rank, group in df.groupby("Class"):
+    group.to_csv(f"{output_file_path}Class/{class_rank}.csv", index=False)
+
+for phylum, group in df.groupby("Phylum"):
+    group.to_csv(f"{output_file_path}Phylum/{phylum}.csv", index=False)
+
+for kingdom, group in df.groupby("Kingdom"):
+    group.to_csv(f"{output_file_path}Kingdom/{kingdom}.csv", index=False)
+
+for realm, group in df.groupby("Realm"):
+    group.to_csv(f"{output_file_path}Realm/{realm}.csv", index=False)
