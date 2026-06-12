d1e5edde00d2bb86342bd741478b8a32cb4681d3
Blessy Antony <blessy.antony@tsgforce.com>
2026-06-11 23:14:54 -0400

    Prott5 prediction

--- src/pipelines/virus_host_prediction_training/fine_tuning_external_pipeline.py
+++ src/pipelines/virus_host_prediction_training/fine_tuning_external_pipeline.py
@@ -73,13 +73,17 @@ def execute(config):
                                                                                 fine_tune_settings["train_proportion"],
                                                                                 split_input_col=fine_tune_settings["split_input_col"],
                                                                                 label_col=label_col)
+                val_df, test_df = dataset_utils.split_dataset_based_on_column(test_df, input_split_seeds[iter],
+                                                                              0.5,
+                                                                              split_input_col=fine_tune_settings["split_input_col"],
+                                                                              label_col=label_col)
             else:
                 # full df into training and testing datasets in the ratio configured in the config file
                 train_df, test_df = dataset_utils.split_dataset_stratified(df, input_settings["split_seeds"][iter],
                                                                            fine_tune_settings["train_proportion"], stratify_col=label_col)
-            # split testing set into validation and testing datasets in equal proportion
-            # so 80:20 will now be 80:10:10
-            val_df, test_df = dataset_utils.split_dataset_stratified(test_df, input_split_seeds[iter], 0.5, stratify_col=label_col)
+                # split testing set into validation and testing datasets in equal proportion
+                # so 80:20 will now be 80:10:10
+                val_df, test_df = dataset_utils.split_dataset_stratified(test_df, input_split_seeds[iter], 0.5, stratify_col=label_col)
         else:
             # used in zero shot evaluation, where split_input=False in fine_tune_settings and mode=test in task
             test_df = df
